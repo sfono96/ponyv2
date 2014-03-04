ca9c27c4ffa9b0eef5c8e506c85d6fe72727b905
sfono96 <sfono96@gmail.com>
2014-03-04 10:07:59 -0700

    Added Procfile and requirements.txt

--- pgdb.py
+++ pgdb.py
@@ -4,7 +4,7 @@ from math import trunc
 
 ############ DB CONNECTION ############ 
 
-production = False # set this to true if pushing changes to production server (heroku) else keep false on development box
+production = True # set this to true if pushing changes to production server (heroku) else keep false on development box
 
 if production == False:
 
